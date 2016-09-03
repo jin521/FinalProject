eb8160ebb84d54bdde55195131b5915f597e5f1e
jin521 <jinlc220@126.com>
2016-09-03 18:13:38 +1000

    add sessionStorage

--- Happyplanet/app/assets/javascripts/home.js
+++ Happyplanet/app/assets/javascripts/home.js
@@ -3,8 +3,24 @@ $(document).ready(function () {
 
 
 
+
+// refer to https://developer.mozilla.org/en/docs/Web/API/Window/sessionStorage
+
+
+      // Get saved data from sessionStorage
+      var data = sessionStorage.getItem('displayed');
+      //if data does not exit, meaning not in session storage,
+      if(!data){
+        $("#entersite").css("display","block")
+      }
+
+
+
       $( "#entersite" ).on('click', function() {
          $('#entersite').fadeOut(2000);
+
+         // Save data to sessionStorage, set it to false as a flag - we have shown entersite box before
+         sessionStorage.setItem('displayed', 'true');
       });
 
 
@@ -12,5 +28,12 @@ $(document).ready(function () {
 
         $('.box').slideDown(800);
       });
+
+
+
+
+
+
+
 
 });
